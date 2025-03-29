678fe999091e6eb9585d6043fa2441418f4579ce
nintser <nintser@gmail.com>
2025-03-30 03:46:50 +0700

    fix shebamg in section-2

--- section-2/2.9/exer_2.9_6.sh
+++ section-2/2.9/exer_2.9_6.sh
@@ -1,8 +1,8 @@
-#/bin/bash
+#!/bin/bash
 
 echo 'Привет! Мое имя '$USER
 echo 'Сейчас я нахожусь в '$PWD
 echo 'Мой домашний каталог '$HOME
-#echo 'Моя ОС '; cat /etc/os-release
-echo 'Моя ОС '; sw_vers
+echo 'Моя ОС '; cat /etc/os-release
+#echo 'Моя ОС '; sw_vers
 /bin/echo -n 'Мое время работы '; uptime
